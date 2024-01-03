956335e359218781a545986594ce46c516ae10bb
ozoizme <ajy020126@gmail.com>
2024-01-03 17:06:22 +0900

    commit
    react 하는중

---
+++ Adventure/src/main/java/com/ajy/adventure/member/model/vo/Member.java
@@ -0,0 +1,17 @@
+package com.ajy.adventure.member.model.vo;
+
+import java.sql.Date;
+
+public class Member {
+	
+	private int memNo; //MEM_NO	NUMBER
+	private String memName; //MEM_NAME	VARCHAR2(100 BYTE)
+	private String memNickName; //MEM_NICKNAME	VARCHAR2(100 BYTE)
+	private String memId; //MEM_ID	VARCHAR2(50 BYTE)
+	private String memPwd; //MEM_PWD	VARCHAR2(50 BYTE)
+	private String memEmail; //MEM_EMAIL	VARCHAR2(80 BYTE)
+	private String memCountry; //MEM_COUNTRY	VARCHAR2(50 BYTE)
+	private Date enrollDate; //ENROLL_DATE	DATE
+	private char memStatus; //MEM_STATUS	CHAR(1 BYTE)
+
+}

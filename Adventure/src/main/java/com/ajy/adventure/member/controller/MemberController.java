7de3ba96eaeda52e0beaf6a5b3b9db6754ec5ea7
ozoizme <ajy020126@gmail.com>
2024-01-09 07:46:37 +0900

    commit
    commit

--- Adventure/src/main/java/com/ajy/adventure/member/controller/MemberController.java
+++ Adventure/src/main/java/com/ajy/adventure/member/controller/MemberController.java
@@ -1,9 +1,15 @@
 package com.ajy.adventure.member.controller;
 
+import java.nio.charset.Charset;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ajy.adventure.member.model.service.MemberService;
@@ -13,18 +19,19 @@ import lombok.RequiredArgsConstructor;
 
 @CrossOrigin("*")
 @RestController
+@RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
 	
 	private final MemberService memberService = null;
 	
-	@PostMapping("insert.me")
+	@PostMapping
 	public ResponseEntity<String> insertMember(@RequestBody Member member) {
 		
-		memberService.insertMember(member);
-	
-
-		return;
+		String result = memberService.insertMember(member) != 0 ? "success" : "fail";
+		HttpHeaders header = new HttpHeaders();
+		header.setContentType(new MediaType("text","html", Charset.forName("UTF-8")));
+		return new ResponseEntity<String>(result, header, HttpStatus.OK);
 	}
 	
 
